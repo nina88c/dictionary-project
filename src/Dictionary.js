9bf3ff8788f5d6dc3a4dd17dddfc90d4b3a8e041
nina88c <131000810+nina88c@users.noreply.github.com>
2023-11-24 12:22:22 +0000

    api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
 
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
   }
+
+  //https://www.shecodes.io/learn/apis/dictionary
+  let apiKey = "0f605ca33b8d413fa995ab3t060267od";
+  let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
