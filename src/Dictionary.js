cde2814ce14c6e2eaa10e8882b664f5828c19fec
nina88c <131000810+nina88c@users.noreply.github.com>
2023-11-24 16:48:24 +0000

    Results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,35 +1,36 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
-import Results from "./Results.js";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
-
-  function handleResponse(response) {
-    console.log(response.data);
-  }
+  let [keyword, setKeyword] = useState(null);
+  let [results, setResults] = useState(null);
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+    //https://www.shecodes.io/learn/apis/dictionary
+    let apiKey = "f6baffbd4tea0o94df351e9ea09c0c43";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
-  //https://www.shecodes.io/learn/apis/dictionary
-  let apiKey = "bd79ao40tde3dec118ca46bc3e6dd55f";
-  let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-  axios.get(apiUrl).then(handleResponse);
+  function handleResponse(response) {
 
-  function handleKeywordChange(event) {
-    setKeyword(event.target.value);
+    setResults(response.data);
   }
+
+   function handleKeywordChange(event) {
+      setKeyword(event.target.value);
+  }
+
 
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
-      <Results />
+      <Results results={results} />
     </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-export default function Results() {
+export default function Results(props) {
+  if (props.results) {
     return (
-     <div className="Results">
-        hello from results
-    </div>   
+      <div className="Definitions">
+        <h2>{props.results.word}</h2>
+        <ul>
+          {props.results.meanings.map(function (meaning, index) {
+            return <li key={index}>{meaning.definition}</li>;
+          })}
+        </ul>
+      </div>
     );
-    
+  } else {
+    return "Loading...";
+  }
 }
