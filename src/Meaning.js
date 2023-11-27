457f2fd6ade7fddd03243b63014ea7c5c853d180
nina88c <131000810+nina88c@users.noreply.github.com>
2023-11-27 16:53:15 +0000

    synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Synonyms from "./Synonyms"
 
 export default function Meaning(props) {
-   console.log(props.meaning);
-   return (
-     <div className="Meaning">
-       <h3> {props.meaning.partOfSpeech}</h3>
-       <p>{props.meaning.defintion}</p>
-       <p>{props.meaning.example}</p>
-     </div>
-   );
+  console.log(props.meaning.example);
+  return (
+    <div className="Meaning">
+      <h3> {props.meaning.partOfSpeech}</h3>
 
+      <div className="definition">{props.meaning.definition}</div>
+      
+      <div className="example">{props.meaning.example}</div>
+      <Synonyms synonyms={props.meaning.synonyms} />
+    </div>
+  );
 }
